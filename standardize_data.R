18b424097307a36f0fce49af34be7ada44cb005d
Anthony Williams <afwillia@scharp.org>
2017-09-28 17:12:35 -0700

    Added ability to format columns

--- standardize_data.R
+++ standardize_data.R
@@ -10,7 +10,7 @@
 ##################################
 
 
-standardize_data <- function(data, keep_cols=NA, rename=list(), add_cols=list()) {
+standardize_data <- function(data, keep_cols=NA, rename=list(), add_cols=list(), fmt_cols=list()) {
   
   # Read in data based on filetype
   if (any(class(data) == "data.frame")) ds <- data
@@ -24,6 +24,12 @@ standardize_data <- function(data, keep_cols=NA, rename=list(), add_cols=list())
   
   # Add add_cols to data
   if (length(add_cols)>0) ds <- cbind(ds, add_cols)
+  
+  # Format columns
+  if (length(fmt_cols) > 0) {
+    fmt <- lapply(fmt_cols, function(x) eval(parse(text=x), envir=ds))
+    ds <- cbind(ds, fmt)
+  }
   
   # After adding columns, check if any subset columns are not present. Fill with NA and warn
   if (!all(keep_cols %in% names(ds))){
